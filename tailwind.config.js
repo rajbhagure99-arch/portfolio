991ae8d9978dc1158a3fceeaba33fc71b4301811
rajbhagure99-arch <rajbhagure99@gmail.com>
2026-06-21 23:57:49 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'dark-bg': '#0f1115',
+        'dark-surface': '#171a1f',
+        'neon': '#00df8f',
+        'neon-dark': '#00b374',
+      },
+    },
+  },
+  plugins: [],
+}
